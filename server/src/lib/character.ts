44b533b6dea8fc6dffcb690125ee5f97fa8d25bc
HearthR <anny6159@naver.com>
2019-10-23 16:50:50 +0900

    character 클래스 스탯 타입 일부 변경

--- server/src/lib/character.ts
+++ server/src/lib/character.ts
@@ -1,14 +1,16 @@
 type statObject = {attack : number, hp : number, mp : number, str : number, dex : number, int : number, luk : number};
+type statRatioObject = {type : statType, value : number}
 type cubeOptionObject = {mainStat : number, subStat : number, attack : number};
 type criticalObject = {min : number, max : number, damage : number};
 type itemObject = any; // todo
-
+type statType = | "str" | "dex" | "int" | "luk" | "hp" | "mp" | "all" 
+                | "patk" /* physical attack */ | "matk" /* magical attack */ | "boss" | "igndef" /* ignore defense */ | "dmg";
 
 class character {
     lv : number;
     job : string;
     stats : statObject;
-    cubeOptions : cubeOptionObject;
+    cubeOptions : statRatioObject
     weaponConst : number;
     weaponExpert : number;
     bossAttack : number;
